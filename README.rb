4a1f422ec9a2372d1e79513e8446437b9335fe32
timchen777 <timchen7@yahoo.com>
2017-04-14 01:26:10 +0000

    14~21: setup MongoDB, mongoose, and two models: messages,user

--- README.rb
+++ README.rb
@@ -56,3 +56,102 @@ http://node2-timchen7.c9users.io:8080/message/submitText
 A NodeJS view
 submitText 
            Submit <--- submit "submitText"
+--- routes/app.js--------
+var express = require('express');
+var router = express.Router();
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+router.get('/message/:msg', function(req, res, next ) {
+    res.render('node', {message: req.params.msg});
+});
+router.post('/message', function (req, res, next) {
+    var message = req.body.message;
+    res.redirect('/message/'+message);
+});
+module.exports = router;
+----------- views/node.hbs ----
+<h1>A NodeJS view</h1>
+{{ message }}
+<form action="/message" method="post">
+    <input type="text" name="message">
+    <button type="submit">Submit</button>
+</form>
+===============第 2 節，講座 14=======================
+This Course if of course not mainly about NodeJS. So if you want to dive 
+deeper into it, the following Resources should be helpful.
+#1: Official NodeJS Page (also dive into the Docs there!): 
+    https://nodejs.org/en/
+#2: An Absolute Beginner Guide to NodeJS: 
+    http://blog.modulus.io/absolute-beginners-guide-to-nodejs
+#3: Official ExpressJS Page (check out the "Getting Started" and other Guide there!): 
+    http://expressjs.com/
+#4: Creating a Basic NodeJS + ExpressJS App from Scratch: 
+    https://shapeshed.com/creating-a-basic-site-with-node-and-express/
+============================= 16: Setting up MongoDB =======================
+https://www.mongodb.com
+https://community.c9.io/t/setting-up-mongodb/1717
+Installing MongoDB on a Cloud9 workspace
+To install MongoDB in your workspace, you can open a terminal and run the following command:
+--------1--------------
+sudo apt-get install -y mongodb-org
+Running MongoDB on a Cloud9 workspace
+MongoDB is preinstalled in your workspace. To run MongoDB, run the following below (passing the correct parameters to it). Mongodb data will be stored in the folder data.
+---------2-------------
+$ mkdir data
+$ echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
+$ chmod a+x mongod
+You can start mongodb by running the mongod script on your project root:
+$ ./mongod
+---------explain ---------
+MongoDB parameters used:
+--dbpath=data - Because it defaults to /var/db (which isn't accessible)
+--nojournal - Because mongodb usually pre-allocates 2 GB journal file (which exceeds Cloud9 disk space quota)
+--bind_ip=$IP - Because you can't bind to 0.0.0.0
+--rest - Runs on default port 28017
+You should use the host $IP instead of localhost as your driver connection URL. For example, in Node, it is: process.env.IP
+Shell Access
+To access a shell prompt for the above MongoDB run the following.
+$ mongo
+--------------do it ---------------
+timchen7:~/seed-project (master) $ sudo apt-get install -y mongodb-org
+timchen7:~/seed-project (master) $ mkdir data
+timchen7:~/seed-project (master) $ echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
+timchen7:~/seed-project (master) $ ls
+README.rb  assets/  data/   node_modules/  public/  tsconfig.aot.json  views/                    webpack.config.dev.js
+app.js     bin/     mongod  package.json   routes/  tsconfig.json      webpack.config.common.js  webpack.config.prod.js
+timchen7:~/seed-project (master) $ cat mongod
+mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"
+timchen7:~/seed-project (master) $ chmod a+x mongod
+=============== 17: Start up MongoDB server 'mongod' =============
+timchen7:~/seed-project (master) $ ./mongod  
+2017-04-14T00:09:24.766+0000 [initandlisten] MongoDB starting : pid=1617 port=27017 dbpath=data 64-bit host=timchen7-node2-4719994
+2017-04-14T00:09:24.767+0000 [initandlisten] options: { net: { bindIp: "0.0.0.0", http: { RESTInterfaceEnabled: true, enabled: true } }, storage: { dbPath: "data", journal: { enabled: false } } }
+================== sing the MongoDB Shell Client 'mongo'=============
+open another terminal
+timchen7:~/seed-project (master) $ mongo
+MongoDB shell version: 2.6.12
+connecting to: test
+Welcome to the MongoDB shell.
+2017-04-14T00:09:24.762+0000 **          enabling http interface
+> use node-angular
+switched to db node-angular
+> show collections
+=== 9. Using Mongoose to work with Data and Models ====
+(Mongoose is advanced mongo DB client framework/package 
+between NodeJS app and MongoDB, to handle 
+schemas,models,validation,intuitive Database Operations)
+============= 20. Using Mongoose in NodeJS =========
+timchen7:~/seed-project (master) $ npm install --save mongoose
+mongoose will be added in package.json
+   "mongoose": "^4.9.4",
+add "var mongoose = require('mongoose');" and 
+"mongoose.connect('http://node2-timchen7.c9users.io:27017/node-angular');" into app.js
+port 27017 is found out from after running ./mongod above, as following
+2017-04-14T00:09:24.766+0000 [initandlisten] MongoDB starting : pid=1617 port=27017 dbpath=data 64-bit host=timchen7-node2-4719994
+=============21. Creating the Project Models with Mongoose================
+1.create message.js and users.js model in new created 'models' folder 
+2.timchen7:~/seed-project (master) $ npm install --save mongoose-unique-validator
+(   "mongoose-unique-validator": "^1.0.5", will be added to package.json )
+3. add "var mongooseUniqueValidator = require('mongoose-Unique-Validator');" and 
+       "schema.plugin(mongooseUniqueValidator);" to user.js model
